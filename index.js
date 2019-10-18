e44163279e7bc67ddafd2e66569ebeacbfa2d1bf
nikita tarasov <nikita@mygento.ru>
2019-10-18 17:17:28 +0300

    extend rules

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-"use strict";
+'use strict';
 module.exports = {
-    rules: {
-      "semi": ["error", "always"]
-    }
-}
+  extends: 'eslint:recommended',
+  env: {
+    'node': true,
+    'browser': true,
+    'jquery': true,
+    'amd': true,
+    'prototypejs': true
+  },
+  rules: {
+    indent: ['error', 2],
+    'no-trailing-spaces': 'error',
+    'keyword-spacing': [
+      'error',
+      {
+        before: true,
+        after: true,
+      },
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    'no-multiple-empty-lines': 'error',
+    'no-unused-vars': ['error', { args: 'all', argsIgnorePattern: '^_.+' }],
+    quotes: [
+      'error',
+      'single',
+    ],
+    semi: [
+      'error',
+      'always',
+    ],
+    'space-before-blocks': [
+      'error',
+      'always',
+    ],
+  },
+};
