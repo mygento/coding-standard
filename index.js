fb28b328e836e8679a90da09c0d53f401a184fbc
nikita tarasov <nikita@mygento.ru>
2022-06-27 17:53:24 +0300

    beta3

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 'use strict';
 module.exports = {
-  "extends": "stylelint-config-standard",
-  "rules": {
-    "at-rule-no-unknown": null,
-    "scss/at-rule-no-unknown": true,
-    "at-rule-empty-line-before": null,
-    "declaration-empty-line-before": null,
-    "block-closing-brace-newline-after": null,
-    "scss/at-else-closing-brace-newline-after": "always-last-in-chain"
+  'extends': 'stylelint-config-standard',
+  'rules': {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'at-rule-empty-line-before': null,
+    'declaration-empty-line-before': null,
+    'block-closing-brace-newline-after': null,
+    'scss/at-else-closing-brace-newline-after': 'always-last-in-chain',
+    'color-named': 'never',
+    // 'declaration-use-variable': ''
+    'declaration-no-important': true,
+    'max-nesting-depth': [2, {ignore: 'pseudo-classes'}],
+    'string-quotes': 'single'
+    // unit-allowed-list
   },
-  "plugins": [
-    "stylelint-scss"
+  'plugins': [
+    'stylelint-scss'
   ]
 };
