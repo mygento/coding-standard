f96eaa52d2f9510cb2769135ac36f9f6c06b4879
Maksim Artemenko <maxim.a@overdose.digital>
2024-03-11 07:40:44 +0300

    use magento_root/generated directory for autogenerated code

--- stan/autoload.php
+++ stan/autoload.php
@@ -12,7 +12,7 @@ use Magento\Framework\TestFramework\Unit\Autoloader\GeneratedClassesAutoloader;
 
 if (!defined('TESTS_TEMP_DIR')) {
     //phpcs:ignore Magento2.Functions.DiscouragedFunction
-    define('TESTS_TEMP_DIR', dirname(__DIR__) . '/../../generate');
+    define('TESTS_TEMP_DIR', dirname(__DIR__) . '/../../..');
 }
 
 $generatorIo = new Io(
